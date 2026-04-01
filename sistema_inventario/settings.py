baf5e0323cb8b8711cb5063ec85a78d3d706de42
Luis Mosqueda <isaac.mosquedag@gmail.com>
2026-04-01 17:44:34 -0600

    ok

--- sistema_inventario/settings.py
+++ sistema_inventario/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-p&b82aq^^#@-crz5%b8o1=ol*%evjm@_4vn(beud^4(m3%b@%y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['suite.crossovermx.com', 'localhost', '127.0.0.1']
 
