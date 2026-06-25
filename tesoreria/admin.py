320994f3f3312e422ee4dea7dd13fddcba04da03
Luis Mosqueda <isaac.mosquedag@gmail.com>
2026-06-25 16:30:14 -0600

    up3

--- tesoreria/admin.py
+++ tesoreria/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from .models import CajaBanco, PagoPedido, Ingreso, PagoCompra, Egreso
 
-# Register your models here.
+@admin.register(CajaBanco)
+class CajaBancoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nombre', 'tipo', 'moneda', 'sucursal', 'activo')
+    list_filter = ('tipo', 'moneda', 'activo', 'sucursal')
+    search_fields = ('nombre', 'banco_nombre', 'cuenta')
+
+@admin.register(PagoPedido)
+class PagoPedidoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'pedido', 'fecha_pago', 'forma_pago', 'caja_banco', 'monto', 'moneda', 'estado')
+    list_filter = ('forma_pago', 'moneda', 'estado', 'sucursal')
+    search_fields = ('pedido__id', 'referencia')
+
+@admin.register(Ingreso)
+class IngresoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'fecha', 'concepto', 'monto', 'moneda', 'forma_pago', 'caja_banco', 'estado')
+    list_filter = ('forma_pago', 'moneda', 'estado', 'sucursal')
+    search_fields = ('concepto', 'referencia')
+
+@admin.register(PagoCompra)
+class PagoCompraAdmin(admin.ModelAdmin):
+    list_display = ('id', 'orden_compra', 'fecha_pago', 'forma_pago', 'caja_banco', 'monto', 'moneda', 'estado')
+    list_filter = ('forma_pago', 'moneda', 'estado', 'sucursal')
+    search_fields = ('orden_compra__id', 'referencia')
+
+@admin.register(Egreso)
+class EgresoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'fecha', 'concepto', 'monto', 'moneda', 'forma_pago', 'caja_banco', 'estado')
+    list_filter = ('forma_pago', 'moneda', 'estado', 'sucursal')
+    search_fields = ('concepto', 'referencia')
